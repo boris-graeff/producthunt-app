48f773f50ae183f61b00046fdc04e484d375c925
Boris Graeff <boris.graeff@gmail.com>
2018-11-14 16:00:17 +0100

    Add axios and set up producthunt api calls

---
+++ src/api/producthunt.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const token = '69bbf887777163aeab050ade0ced0c3b47f781ad6a17aa2e92471b7a67a4da2d'
+
+const client = axios.create({
+  baseURL: 'https://api.producthunt.com/v1',
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+})
+
+export const getPosts = () => client.get(`/posts`)
