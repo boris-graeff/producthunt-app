59af9b874a3ef6b80314d67619e68a7bd5f02869
Boris Graeff <boris.graeff@gmail.com>
2018-11-14 15:12:43 +0100

    Create store modules for posts

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import posts from './modules/posts'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  namespaced: true,
+  modules: {
+    posts
   }
 })
